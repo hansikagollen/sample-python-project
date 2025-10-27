5c6e3576b346f11de33c2145fa13bc8098cbbd5b
hansikagollen <146338862+hansikagollen@users.noreply.github.com>
2025-10-27 15:53:42 +0530

    Update testsample.py

--- testsample.py
+++ testsample.py
@@ -1,8 +1,2 @@
-import requests
-
-def get_status():
-    response = requests.get("https://httpbin.org/status/200")
-    return response.status_code
-
-if __name__ == "__main__":
-    print("Status Code:", get_status())
+def test_addition():
+    assert 2 + 3 == 5
